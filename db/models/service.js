194f337db6970627ccd27d40761a7843ab14e176
Yeonju Park <yeonjupark95@gmail.com>
2022-04-16 15:48:48 -0700

    created db model for service

---
+++ db/models/service.js
@@ -0,0 +1,34 @@
+const client = require("../client");
+
+const createService = async ({ name, description, category, price }) => {
+  try {
+    const {
+      row: [service],
+    } = await client.query(
+      `
+        INSERT INTO service (name, description, category, price)
+      `,
+      [name, description, category, price]
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getServiceById = async (id) => {
+  try {
+    const { rows: service } = await client.query(
+      `SELECT * FROM service
+            WHERE id = $1;
+            `,
+      [id]
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  getServiceById,
+  createService,
+};
