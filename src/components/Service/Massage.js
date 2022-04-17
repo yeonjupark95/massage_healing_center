50844f5e6dd8a03f912756a2ea542dd52614e28d
Yeonju Park <yeonjupark95@gmail.com>
2022-04-16 22:31:28 -0700

    created Massage menu

--- src/components/Service/Massage.js
+++ src/components/Service/Massage.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Appointment from "./Appointment";
 import "../../style/Service.css";
+import { fetchCategory } from "../../axios-services/index";
 
 const Massage = () => {
-  return <>Massage</>;
+  const [massage, setMassage] = useState([]);
+  const [both, setBoth] = useState([]);
+
+  const handleMassage = async () => {
+    const fetchedMassage = await fetchCategory("massage");
+    setMassage(fetchedMassage);
+  };
+
+  const handleBoth = async () => {
+    const fetchedBoth = await fetchCategory("both");
+    setBoth(fetchedBoth);
+  };
+
+  useEffect(() => {
+    handleMassage();
+    handleBoth();
+  }, []);
+
+  return (
+    <>
+      <Appointment />
+      <div className="menu">
+        <div className="service-menu-title">Massage</div>
+        <div className="service-menu-container">
+          {massage.map((service) => {
+            const { id, name, description, price } = service;
+            return (
+              <div className="service-container">
+                <div className="service-title">
+                  <div className="service-name">{name}</div>
+                  <div className="service-price">{price}</div>
+                </div>
+                <div className="service-description">{description}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="service-menu-title">Package</div>
+        <div className="service-menu-container">
+          {both.map((service) => {
+            const { id, name, description, price } = service;
+            return (
+              <div className="service-container">
+                <div className="service-title">
+                  <div className="service-name">{name}</div>
+                  <div className="service-price">{price}</div>
+                </div>
+                <div className="service-description">{description}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 export default Massage;
