3c208981eb3712b8e65616e8d34482b011e57deb
Yeonju Park <yeonjupark95@gmail.com>
2022-04-17 16:05:08 -0700

    created axios calls for service

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -48,3 +48,66 @@ export const fetchUser = async (token) => {
     console.error(error);
   }
 };
+
+export const createService = async (
+  token,
+  { name, description, category, price }
+) => {
+  try {
+    const { data } = await axios.post(
+      `${BASE_URL}/services/add`,
+      {
+        name,
+        description,
+        category,
+        price,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteService = async (token, serviceId) => {
+  try {
+    const { data } = await axios.delete(`${BASE_URL}/services/${serviceId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateService = async (
+  token,
+  { serviceId, name, description, category, price }
+) => {
+  try {
+    const { data: service } = await axios.patch(
+      `${BASE_URL}/services/${serviceId}`,
+      {
+        name,
+        description,
+        category,
+        price,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return service;
+  } catch (error) {
+    console.error(error);
+  }
+};
