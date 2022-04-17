df557086fbd6c383704e11b20971db914cc9d1dc
Yeonju Park <yeonjupark95@gmail.com>
2022-04-16 23:18:45 -0700

    created utils in api

--- api/users.js
+++ api/users.js
@@ -1,37 +1,46 @@
 const usersRouter = require("express").Router();
 const { getUser, createUser, getUserByUsername } = require("../db");
+const { requireUser } = require("./utils.js");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { JWT_SECRET } = process.env;
 
 usersRouter.post("/login", async (req, res, next) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      return next({
-        name: "MissingCredentialsError",
-        message: "Please supply both a username and password",
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUser({ username, password });
+    if (user) {
+      const token = jwt.sign(user, JWT_SECRET);
+      res.send({
+        user,
+        token,
+        message: `Welcome back ${user.username}! `,
+      });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
       });
     }
-  
-    try {
-      const user = await getUser({ username, password });
-      if (user) {
-        const token = jwt.sign(user, JWT_SECRET);
-        res.send({
-          user,
-          token,
-          message: `Welcome back ${user.username}! `,
-        });
-      } else {
-        next({
-          name: "IncorrectCredentialsError",
-          message: "Username or password is incorrect",
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+usersRouter.get("/admin", requireUser, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = usersRouter;
