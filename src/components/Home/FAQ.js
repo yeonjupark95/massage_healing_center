7f73b0915b78439bb7939a3934c33a8d9070a81a
Yeonju Park <yeonjupark95@gmail.com>
2022-04-17 00:22:59 -0700

    added faq css

--- src/components/Home/FAQ.js
+++ src/components/Home/FAQ.js
@@ -4,7 +4,16 @@ import "../../style/Home.css";
 const FAQ = () => {
 return (
     <>
-    FAQ
+     <div className="faq-container">
+      <div className="faq-header">Frequently Asked Questions</div>
+      <div className="faq-question">What is LDM (Local Dynamic Micro) water drop lifting technology? </div>
+      <div className="faq-answer">
+        Helen has been a Licensed Massage Therapist for over 10 years and is also a licensed aesthetician. <br />
+        As Helen lived in Korea for the majority of her life, she is well aware of the popular technologies used among the aestheticians in Korea.
+        <br />
+        If you have any questions regarding her service, please text her at 214-668-7839.
+      </div>
+    </div>
     </>
 )
 }
